11d0ce78fd64a3b76fae02036b760e7b499e15a5
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-22 16:15:27 -0400

    moved editor into components folder and began working on the recorder component

--- server/api/aws.js
+++ server/api/aws.js
@@ -1,15 +1,15 @@
-const router = require('express').Router()
-let AWS = require('aws-sdk')
-let fs = require('fs')
-let path = require('path')
-let configPath = path.join(__dirname, '../../secrets.js').awsConfig
-module.exports = router
+const router = require('express').Router();
+let AWS = require('aws-sdk');
+let fs = require('fs');
+let path = require('path');
+let configPath = path.join(__dirname, '../../secrets.js').awsConfig;
+module.exports = router;
 
 //set up AWS config
-AWS.config.loadFromPath(configPath)
+AWS.config.loadFromPath(configPath);
 
 // Create an S3 client
-let s3 = new AWS.S3()
+let s3 = new AWS.S3();
 
 //mounted on /api/aws
 //POST route for '/api/aws/upload'
@@ -18,34 +18,34 @@ router.post('/upload', function(req, res) {
   var s3request = {
     Body: '', //actual file turned into a string,
     Bucket: 'replicode',
-    Key: req.files.uploadedFile.name //filename
-  }
+    Key: req.files.uploadedFile.name, //filename
+  };
 
   s3.putObject(s3request, function(err, data) {
     if (err) {
-      console.error(err)
+      console.error(err);
     }
-    res.send('Uploaded successfully')
-  })
-})
+    res.send('Uploaded successfully');
+  });
+});
 
 //GET request for list of objects in bucket
 // '/api/aws/list'
 router.get('/list', function(req, res) {
-  s3.listObjects({Bucket: 'replicode'}, function(err, data) {
+  s3.listObjects({ Bucket: 'replicode' }, function(err, data) {
     if (err) {
-      console.error(err)
+      console.error(err);
     }
-    res.json(data)
-  })
-})
+    res.json(data);
+  });
+});
 
 router.get('/specificFile', (req, res) => {
-  let request = {Bucket: 'replicode', Key: req.param.name}
+  let request = { Bucket: 'replicode', Key: req.param.name };
   s3.getObject(request, (err, data) => {
     if (err) {
-      console.error(err)
+      console.error(err);
     }
-    res.json(data)
-  })
-})
+    res.json(data);
+  });
+});
