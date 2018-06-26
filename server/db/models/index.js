4174eaea069201502fd04407ca242c60555330a3
Chris Perez <chrisaugustusp@gmail.com>
2018-06-26 17:23:43 -0400

    Project and User Associations created

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
-const User = require('./user')
+const User = require('./user');
+const Project = require('./project');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +8,9 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+User.hasMany(Project);
+Project.belongsTo(User);
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +18,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
-}
+  User,
+  Project,
+};
