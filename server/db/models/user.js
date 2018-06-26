6e6fe94b9c4667a3e21ecd8909ea15e064b9202a
Chris Perez <chrisaugustusp@gmail.com>
2018-06-26 17:13:51 -0400

    Created User model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,67 +1,82 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+//const crypto = require('crypto');
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
   },
-  password: {
-    type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
+  isCreator: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
     get() {
-      return () => this.getDataValue('password')
-    }
+      return () => this.getDataValue('isAdmin');
+    },
   },
-  salt: {
-    type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
-    get() {
-      return () => this.getDataValue('salt')
-    }
-  },
-  googleId: {
-    type: Sequelize.STRING
-  }
-})
+  // password: {
+  //   type: Sequelize.STRING,
+  //   // Making `.password` act like a func hides it when serializing to JSON.
+  //   // This is a hack to get around Sequelize's lack of a "private" option.
+  //   get() {
+  //     return () => this.getDataValue('password');
+  //   },
+  // },
+  // salt: {
+  //   type: Sequelize.STRING,
+  //   // Making `.salt` act like a function hides it when serializing to JSON.
+  //   // This is a hack to get around Sequelize's lack of a "private" option.
+  //   get() {
+  //     return () => this.getDataValue('salt')
+  //   }
+  // }
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function(candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-}
+// User.prototype.correctPassword = function(candidatePwd) {
+//   return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+// };
 
 /**
  * classMethods
  */
-User.generateSalt = function() {
-  return crypto.randomBytes(16).toString('base64')
-}
+// User.generateSalt = function() {
+//   return crypto.randomBytes(16).toString('base64');
+// };
 
-User.encryptPassword = function(plainText, salt) {
-  return crypto
-    .createHash('RSA-SHA256')
-    .update(plainText)
-    .update(salt)
-    .digest('hex')
-}
+// User.encryptPassword = function(plainText, salt) {
+//   return crypto
+//     .createHash('RSA-SHA256')
+//     .update(plainText)
+//     .update(salt)
+//     .digest('hex');
+// };
 
 /**
  * hooks
  */
-const setSaltAndPassword = user => {
-  if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
-  }
-}
+// const setSaltAndPassword = user => {
+//   if (user.changed('password')) {
+//     user.salt = User.generateSalt();
+//     user.password = User.encryptPassword(user.password(), user.salt());
+//   }
+// };
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
+// User.beforeCreate(setSaltAndPassword);
+// User.beforeUpdate(setSaltAndPassword);
