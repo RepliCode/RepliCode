ee0e117691843e3d0b4d08be9e731f59b0fa491f
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-27 20:11:57 -0400

    created modal form for submitting a recording

--- client/components/recordingForm.js
+++ client/components/recordingForm.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+} from 'reactstrap';
 
 class RecordingForm extends React.Component {
   constructor(props) {
@@ -32,7 +43,26 @@ class RecordingForm extends React.Component {
           className={this.props.className}
         >
           <ModalHeader toggle={this.toggle}>Recording information</ModalHeader>
-          <ModalBody />
+          <ModalBody>
+            <Form>
+              <Label for="name">Your Name</Label>
+              <Input type="text" name="name" id="recording-author-name" />
+              <Label for="title">Lesson Title</Label>
+              <Input
+                type="text"
+                name="title"
+                id="recording-title"
+                placeholder="Insert title here"
+              />
+              <Label for="description">Description</Label>
+              <Input
+                type="textarea"
+                name="description"
+                id="recording-description"
+                placeholder="Give a brief description of your lesson"
+              />
+            </Form>
+          </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={this.toggle}>
               Submit
