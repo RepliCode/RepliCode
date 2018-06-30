d1f38e1e579162a187a031f6b4041fde3d9259a7
jessdelacruzsantos <32309499+jessdelacruzsantos@users.noreply.github.com>
2018-06-30 16:59:43 -0400

    Docker container time out #27 (#53)
    * Refactored Docker/runCodeSnippet to kill the docker container when 10 seconds have passed. "your code has timed out" will be output to the console
    
    * Refactored /Docker/runSnippetCode to handle escapes and double-quotes

--- server/Docker/runCodeSnippet.js
+++ server/Docker/runCodeSnippet.js
@@ -1,17 +1,36 @@
 var exec = require('child_process').exec;
 
-function runCodeSnippet(codeSnippet) {
+function stopDocker(name) {
   return new Promise((resolve, reject) => {
-    exec(
-      `docker run replicode:latest node evaluate.js "${codeSnippet}"`,
-      { timeout: 5000 },
-      function(err, stdout, stderr) {
+    setTimeout(() => {
+      console.log('We ran the set timeout');
+      exec(`docker kill "${name}"`, (err, stdout, stderr) => {
         if (err !== null) {
           reject(stderr);
         }
+        resolve('Your code timed out!');
+      });
+    }, 10000);
+  });
+}
+function runCodeSnippet(codeSnippet, name) {
+  const cleanerCode = codeSnippet.replace(/["]/g, "'");
+  stopDocker(name)
+    .then(console.log)
+    .catch(console.log);
+  return new Promise((resolve, reject) => {
+    exec(
+      `docker run --rm --name "${name}" replicode:latest node evaluate.js "${cleanerCode}"`,
+      function(err, stdout, stderr) {
+        if (err !== null) {
+          let errorOutput = stderr === '' ? 'Your code timed out!!!!' : stderr;
+          reject(errorOutput);
+        }
+        console.log(stdout);
         resolve(stdout);
       }
     );
   });
 }
+
 module.exports = runCodeSnippet;
