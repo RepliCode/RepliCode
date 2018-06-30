2fc7caaf4da5b9b2683ed756aacb533b16f85a3c
Jessie De La Cruz Santos <jessdelacruzsantos@gmail.com>
2018-06-30 12:38:24 -0400

    Refactored Docker/runCodeSnippet to kill the docker container when 10 seconds have passed. "your code has timed out" will be output to the console

--- server/api/sandBox.js
+++ server/api/sandBox.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const runCodeSnippet = require('../Docker/runCodeSnippet');
 
 router.post('/', async (req, res, next) => {
+  const name = `someSalt_${Date.now()}`;
   try {
-    const evaluation = await runCodeSnippet(req.body.code);
+    const evaluation = await runCodeSnippet(req.body.code, name);
     res.json(evaluation);
   } catch (err) {
     res.json(err);
