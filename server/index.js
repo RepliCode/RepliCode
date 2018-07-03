8c43c6c4b34f796e070bd059781eddbc88930c83
Jon Rosado <jon.rosado42@gmail.com>
2018-07-03 12:49:09 -0400

    Ssl redirect #69 (#71)
    * added heroku-ssl-redirect middleware to server index.js
    
    * added heroku-ssl-redirect middleware to server index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 const app = express();
 const socketio = require('socket.io');
+const sslRedirect = require('heroku-ssl-redirect');
 module.exports = app;
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -44,6 +45,9 @@ passport.deserializeUser(async (id, done) => {
 const createApp = () => {
   // logging middleware
   app.use(morgan('dev'));
+
+  // ssl redirect middleware
+  app.use(sslRedirect());
 
   // body parsing middleware
 
