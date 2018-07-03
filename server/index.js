3e7add7eb663b5ad3fcfce224d0c7f774ea25bba
Nabil Yafai <nabilyafai90@gmail.com>
2018-07-02 21:18:49 -0400

    added subscriber table for teachers to be able to add a subsriber

--- server/index.js
+++ server/index.js
@@ -110,7 +110,7 @@ const startListening = () => {
   require('./socket')(io);
 };
 
-const syncDb = () => db.sync();
+const syncDb = () => db.sync({ force: true });
 
 async function bootApp() {
   await sessionStore.sync();
